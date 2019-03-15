67171d5b55957720d8027966b225f83f92444d0c
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-03-16 00:30:44 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pseudo',
+      version='0.1',
+      description='Pseudocode interpreter prototype',
+      author='Patryk Niedzwiedzinski',
+      author_email='pniedzwiedzinski19@gmail.com',
+     )
