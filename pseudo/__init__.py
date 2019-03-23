047a9ee24a6ec155a98a1e78748c42c7c356fbb6
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-03-23 20:47:37 +0100

    Add module documentation

--- pseudo/__init__.py
+++ pseudo/__init__.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 
+"""pseudocode interpreter
+
+Writing actual code might be hard to understand for new-learners. Pseudocode is a tool
+for writing algorithms without knowing how to code. This module contains classes and
+methods for parsing pseudocode to AST and then evaluating it.
+
+Example:
+    If you installed this module with pip you can run pseudocode from file, i.e. to run
+    `test.pdc` file type::
+
+        $ pseudo test.pdc
+
+    If you want to parse it by your own you will need `pseudo.lexer.Lexer` instance.::
+
+        from pseudo.lexer import Lexer
+
+        lex = Lexer("x := 12")
+
+        expression = lex.read_next()
+        print(expression)
+
+    If lexer reach the end of input, the `parser.lexer.EndOfFile` exception will be raised.
+"""
+
 import sys
 
 from pseudo.lexer import Lexer, EndOfFile
