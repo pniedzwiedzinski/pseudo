f635c114cf7d8be5a09d0e6e55540366135af499
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-11 22:53:27 +0200

    Add deployment script

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -7,15 +7,15 @@ fi
 
 set -x
 
-export GOPATH=$HOME/go # don't forget to change your path correctly!
-export GOROOT=/usr/local/opt/go/libexec
-mkdir -p $GOPATH $GOPATH/src $GOPATH/pkg $GOPATH/bin
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
-
-go get github.com/tcnksm/ghr
-
 if [ "$TRAVIS_BRANCH" -eq "master" ]; then
+    export GOPATH=$HOME/go # don't forget to change your path correctly!
+    export GOROOT=/usr/local/opt/go/libexec
+    mkdir -p $GOPATH $GOPATH/src $GOPATH/pkg $GOPATH/bin
+    export PATH=$PATH:$GOPATH/bin
+    export PATH=$PATH:$GOROOT/bin
+
+    go get github.com/tcnksm/ghr
+
     echo "Starting deployment of pseudo@$VERSION"
     ghr -t ${GITHUB_TOKEN} -u pniedzwiedzinski -r pseudo -c ${TRAVIS_COMMIT} ${VERSION} ./dist/pdc-$(VERSION)-darwin.tar.gz
 fi
