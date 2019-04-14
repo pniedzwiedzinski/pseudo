1d972307f287466c503c8b546e3bc77d3d1876f3
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-14 22:28:40 +0200

    Fix test on windows

--- tests/pseudo_test.py
+++ tests/pseudo_test.py
@@ -17,22 +17,25 @@ def test_main():
         fp.write("pisz 4")
     cmd = "python3 pdc.py t1.pdc"
     if platform.system() == "Windows":
-        cmd.replace("3", "")
+        cmd = cmd.replace("3", "")
     if subprocess.getoutput(cmd) != "4":
+        print(cmd)
         print(subprocess.getoutput(cmd))
         raise AssertionError
     os.remove("t1.pdc")
 
     cmd = "python3 pdc.py -v"
     if platform.system() == "Windows":
-        cmd.replace("3", "")
+        cmd = cmd.replace("3", "")
     if subprocess.getoutput(cmd) != __version__:
+        print(cmd)
         print(subprocess.getoutput(cmd))
         raise AssertionError
 
     cmd = ["python3", "pdc.py"]
     if platform.system() == "Windows":
-        cmd[0].replace("3", "")
+        cmd[0] = cmd[0].replace("3", "")
     if subprocess.run(cmd).returncode == 0:
+        print(cmd)
         print(subprocess.run(cmd).returncode)
         raise AssertionError
